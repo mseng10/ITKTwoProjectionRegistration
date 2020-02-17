b9f24a02bd770e6efb4b709561123edbd0899500
Hans Johnson <hans-johnson@uiowa.edu>
2020-02-17 14:48:40 -0600

    STYLE: Remove redundant void argument lists
    Find and remove redundant void argument lists.

--- include/itkSiddonJacobsRayCastInterpolateImageFunction.h
+++ include/itkSiddonJacobsRayCastInterpolateImageFunction.h
@@ -148,7 +148,7 @@ public:
   EvaluateAtContinuousIndex(const ContinuousIndexType & index) const override;
 
   virtual void
-  Initialize(void);
+  Initialize();
 
   /** Connect the Transform. */
   itkSetObjectMacro(Transform, TransformType);
@@ -220,7 +220,7 @@ protected:
 
 private:
   void
-                   ComputeInverseTransform(void) const;
+                   ComputeInverseTransform() const;
   TransformPointer m_GantryRotTransform; // Gantry rotation transform
   TransformPointer m_CamShiftTransform;  // Camera shift transform camRotTransform
   TransformPointer m_CamRotTransform;    // Camera rotation transform
